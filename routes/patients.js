574405595a4d8a421f48940d648c1decae73b0af
Andrew Krause <aek.krause@gmail.com>
2022-04-10 17:23:16 -0500

    Gave user ability to update a given patient entity.

--- routes/patients.js
+++ routes/patients.js
@@ -12,6 +12,25 @@
  * 
  */
 
+// NOTES: 
+// ============================================================================
+// AFTER YOU FINISH YOUR THREE COMPLEX QUERIES, COME BACK HERE (AND MAYBE
+// TO THE OTHER MAIN ENTITIES YOU CREATED) AND TRY TO DISPLAY THE TREATMENTS
+// THAT EACH PATIENT IS RECEIVING. 
+
+// THIS DATA WILL BE OBTAINED VIA A JOIN
+// BETWEEN THE PATIENT AND TREATMENT TABLES. YOU WILL NEVER DISPLAY ANY OF
+// THE TABLES THAT REPRESENT A RELATIONSHIP BETWEEN TWO OTHER TABLES IN 
+// YOUR DATABASE. YOU WILL SIMPLY DISPLAY THE RESULT(S) OF THE QUERY THAT
+// COMPLETES THE JOIN ON THE INTERFACE OF THE PATIENT PAGE. 
+
+// IF CHANGES ARE
+// MADE TO THE TREATMENTS LISTED FOR EACH PATIENT VIA THE "UPDATE" FEATURE,
+// THEN APPLY THOSE CHANGES TO THE TABLE (NOT THE PATIENT TABLE) THAT 
+// REPRESENTS THE RELATIONSHIP BETWEEN THE PATIENT TABLE AND THE TREATMENT
+// TABLE.
+// ============================================================================
+
 // Add the packages that are required.
 var express = require('express');
 var patientRouter = express.Router();
@@ -30,7 +49,7 @@ patientRouter.get("/patients", function(req, res, next) {
 
     // Create a query to SELECT all of the patients in
     // the Patient table.
-    var sql = "SELECT * FROM PCM.Patient";
+    var sql = `SELECT * FROM PCM.Patient`;
 
     // Get all of the patients from the patients table. (YOU WILL
     // DISPLAY THIS ON THE PATEINTS VIEW PAGE AT SOME POINT!!!)
@@ -46,10 +65,39 @@ patientRouter.get("/patients", function(req, res, next) {
             // display that data.
             res.render("patients", {title: "Patient List", patientData: data});
         }
+    });
+});
 
-        // Otherwise, log the data obtained 
-        // from the database.
-        // return console.log(data); // --> DEBUGGING STATEMENT; DELETE LATER!!!
+// Creat a route to render the patient data that
+// the user wants to edit on a separate page. If
+// the user chooses to change any of the data, 
+// this route will redirect to an update route
+// that will commit the changes to the databases.
+patientRouter.get("/edit_patient/:patientId", function(req, res){
+ 
+    // Create a constant for storing the post ID so that it
+    // can be retrieved from the database.
+    const requestedPatientId = req.params.patientId;
+
+    // Complete a query to obtain the patient that the
+    // user wishes to edit from the Patient table.
+    var sql = `SELECT * FROM PCM.Patient 
+                WHERE PCM.Patient.PatientID = ?`;
+
+    // Complete the query in the database and display the patient
+    // data that the user wants to edit on a page for that patient.
+    database.query(sql, [requestedPatientId], function(error, data, fields) {
+
+        // If there is an error, log the error.
+        if(error) {
+            console.log(error);
+        } else {
+
+            // Otherwise, send the data to the
+            // patients.ejs page in order to 
+            // allow the user to edit that data.
+            res.render("edit_patient", {title: "Patient Edit", patientEdit: data});
+        }
     });
 });
 
@@ -63,22 +111,190 @@ patientRouter.post("/new_patient", function(req, res){
     // ADD MORE...
 });
 
+
 // Create a post request for when the user wants to
 // update a given patient.
 patientRouter.post("/update_patient", function(req, res){
 
-    // ADD MORE...YOU NEED TO GO TO A PAGE WHERE YOU 
-    // CAN QUERY FOR THE PATIENT YOU WANT TO UPDATE
-    // BY SEARCHING BASED ON THEIR UNIQUE IDENTIFIER,
-    // RENDERING THE INFORMATION ON THE SCREEN,
-    // AND GIVING THE USER THE ABILITY TO EDIT THE 
-    // FIELDS OF THE PATIENT IF THEY WISH TO DO SO.
-    // PROVIDE CANCEL AND UPDATE BUTTONS TO EITHER
-    // CANCEL MAKING CHANGES OR TO APPLY THE CHANGES.
+    // ================================================================
+    // The function ensures that the date attributes are 
+    // entered properly into the database if/when the user 
+    // updates the data.
+    function simplifyDateShorthand(eventDate) {
 
-    var updatePatient = req.body.patientidentifier;
-    console.log(updatePatient);
+        // Check if the date being entered is undefined.
+        // (This would indicate that the patient does
+        // not have an end date)
+        if(!eventDate) {
+            return null;
+        }
+
+        // First split the event into the components that directly 
+        // involve the numberical representations of a date. 
+        var date = eventDate.toString().split(" "); 
+    
+        // Create variables for working with
+        // the nubmers in the date variable.
+        var getMonth = date[1];
+        var month = "";
+        var day = date[2];
+        var year = date[3];
+        var simplifiedDate = "";
+
+        // --> DEBUGGING STATEMENTS; DELETE LATER!!!
+        // console.log("DATE: " + date);
+        // console.log("MONTH: " + getMonth);
+        // console.log("DAY: " + day);
+        // console.log("YEAR: " + year);  
+
+        // Use a series of conditionals to determine 
+        // what the name of the month is based on the 
+        // numerical representation of it. 
+        if(getMonth.localeCompare('Jan') == 0) { 
+            month = '01';
+        } else if(getMonth.localeCompare('Feb') == 0) {
+            month = '02';
+        } else if(getMonth.localeCompare('Mar') == 0) {
+            month = '03'; 
+        } else if(getMonth.localeCompare('Apr') == 0) {
+            month = '04';
+        } else if(getMonth.localeCompare('May') == 0) {
+            month = '05';
+        } else if(getMonth.localeCompare('Jun') == 0) {
+            month = '06';
+        } else if(getMonth.localeCompare('Jul') == 0) {
+            month = '07'; 
+        } else if(getMonth.localeCompare('Aug') == 0) { 
+            month = '08'; 
+        } else if(getMonth.localeCompare('Sep') == 0) {
+            month = '09';
+        } else if(getMonth.localeCompare('Oct') == 0) {
+            month = '10';
+        } else if(getMonth.localeCompare('Nov') == 0) {
+            month = '11';
+        } else if(getMonth.localeCompare('Dec') == 0) {
+            month = '12';
+        } else {
+            month = 'ERROR';
+        } 
+    
+        // Create the simplified date and return it.
+        simplifiedDate = year + "-" + month + "-" + day;
+        return simplifiedDate;
+    }
+    // ================================================================
+
+    // Declare a variable for the patient identifier.
+    var updatePatientID = req.body.defaultpatientid;
+
+    // Declare variables for the values that will be passed into the
+    // SQL update statement below. A series of conditionals before that
+    // SQL update statement determine whether the variables will contain
+    // values entered into the input or the default values.
+    var patientFirstName = req.body.patientfirstname;
+    var patientMiddleName = req.body.patientmiddlename;
+    var patientLastName = req.body.patientlastname;
+    var patientBirthdate = req.body.patientbirthdate; 
+    var patientSex = req.body.patientsex;
+    var patientHeight = req.body.patientheight;
+    var patientWeight = req.body.patientweight;
+    var patientDescription = req.body.patientdescription;
+    var patientPhone = req.body.patientphone;
+    var patientEmail = req.body.patientemail;
+    var patientStartDate = req.body.patientstartdate;
+    var patientEndDate = req.body.patientenddate;
+
+    // Include a series of conditionals to determine if any of
+    // the default values need to be utilized in the process of
+    // updating the patient in the database.
+    if(!patientFirstName) {
+        patientFirstName = req.body.defaultpatientfirstname;
+    }
+    if(!patientMiddleName) {
+        patientMiddleName = req.body.defaultpatientmiddlename;
+    }
+    if(!patientLastName) {
+        patientLastName = req.body.defaultpatientlastname;
+    }
+    if(!patientBirthdate) {
+        patientBirthdate = simplifyDateShorthand(req.body.defaultpatientbirthdate);
+    } else {
+        patientBirthdate = req.body.patientbirthdate.toString().split(" ");  
+    }
+    if(!patientSex) {
+        patientSex = req.body.defaultpatientsex;
+    }
+    if(!patientHeight) {
+        patientHeight = req.body.defaultpatientheight;
+    }
+    if(!patientWeight) {
+        patientWeight = req.body.defaultpatientweight;
+    }
+    if(!patientDescription) {
+        patientDescription = req.body.defaultpatientdescription;
+    }
+    if(!patientPhone) {
+        patientPhone = req.body.defaultpatientphone;
+    }
+    if(!patientEmail) {
+        patientEmail = req.body.defaultpatientemail;
+    }
+    if(!patientStartDate) {
+        patientStartDate = simplifyDateShorthand(req.body.defaultpatientstartdate);
+    } else {
+        patientStartDate = req.body.patientstartdate.toString().split(" ");  
+    }
+    if(!patientEndDate) {
+        patientEndDate = simplifyDateShorthand(req.body.defaultpatientenddate);
+    } else {
+        patientEndDate = req.body.patientenddate.toString().split(" ");  
+    }
+
+    // Include the SQL query that will update the patient entity
+    // in the patient table.
+    var sql = `UPDATE PCM.Patient 
+                SET PatientNotes = ?, PatientFirstName = ?, PatientMiddleName = ?, PatientLastName = ?, PatientBirthdate = ?, PatientSex = ?, PatientWeight = ?, PatientPhone = ?, PatientEmail = ?, PatientHeight = ?, PatientStartDate = ?, PatientEndDate = ?
+               WHERE PatientID = ?;`;
+
+    // Complete the query in the database and display the patient
+    // data that the user wants to edit on a page for that patient.
+    database.query(sql, [patientDescription, 
+                         patientFirstName, 
+                         patientMiddleName, 
+                         patientLastName, 
+                         patientBirthdate, 
+                         patientSex, 
+                         patientWeight, 
+                         patientPhone, 
+                         patientEmail, 
+                         patientHeight, 
+                         patientStartDate, 
+                         patientEndDate, 
+                         updatePatientID], function(error, data, fields) {
+
+        // If there is an error, log the error.
+        if(error) {
+            console.log(error);
+        } else {
+
+            // Redirect the route back to the main patients page.
+            // --> MAYBE LATER INCLUDE A FLASH MESSAGE TO INDICATE
+            // --> THAT THE PATIENT WAS SUCCESSFULLY UPDATED!!!
+            res.redirect("/patients");
+        }
+    });
 });
+
+
+
+
+
+
+
+
+
+
+
 
 // Create a post request for when the user wants to
 // remove a given patient.
